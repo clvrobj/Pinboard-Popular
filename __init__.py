644e5cf6f305750a291969288c44489174f7f65d
clvrobj <clvrobj@gmail.com>
2012-04-06 00:25:52 +0800

    Never tweet a link twice.

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ from time import sleep
 import random
 import urllib2
 from lxml import etree
+from pymongo import Connection
 from twitter_oauth_op import post2twi
 from douban_oauth_op import post2douban
 
@@ -15,7 +16,21 @@ xml_keys = {'item': '{http://purl.org/rss/1.0/}item',
             'title': '{http://purl.org/rss/1.0/}title',
             'link': '{http://purl.org/rss/1.0/}link'}
 pinbpop_url = 'http://feeds.pinboard.in/rss/popular/'
-    
+
+def init_mongo():
+    from local_conf import DATABASE
+    connection = Connection(host=DATABASE['HOST'], port=DATABASE['PORT'], network_timeout=2)
+    db = connection[DATABASE['NAME']]
+    db.authenticate(DATABASE['USER'], DATABASE['PASSWORD'])
+    collection = db['bookmarks']
+    return connection, db, collection
+
+def check_tweeted_link(collection, link):
+    links = collection.find({'link':link})
+    return True if links.count() > 0 else False
+
+def save_tweeted_link(collection, link, title):
+    collection.insert({'link':link, 'title':title})
 
 def download_pop_xml():
     url = pinbpop_url
@@ -29,18 +44,22 @@ def download_pop_xml():
     w.write(data)
     w.close()
 
-def parse_pop_xml(count):
+def parse_pop_xml(collection, count):
     doc = etree.parse(file_name)
     items = doc.findall(xml_keys['item'])
-    random.shuffle(items) # disrupt the list sort
     ret = []
-    for item in items[:count]:
-        title = item.find(xml_keys['title']).text.encode('utf-8').strip()
+    i = 0
+    while len(ret) < count and i < len(items):
+        item = items[i]
         link = item.find(xml_keys['link']).text.encode('utf-8').strip()
-        ret.append({'title':title, 'link':link})
+        if not check_tweeted_link(collection, link):
+            title = item.find(xml_keys['title']).text.encode('utf-8').strip()
+            ret.append({'title':title, 'link':link})
+        i += 1
     return ret
 
-def add_status(title, link):
+def add_status(collection, title, link):
+    save_tweeted_link(collection, link, title)
     content = '%s %s' % (title, link)
     if len(content) > 140:
         tlen = len(content) - len(link) - 1
@@ -51,9 +70,11 @@ def add_status(title, link):
 
 
 if __name__ == '__main__':
+    conn, db, collection = init_mongo()
     download_pop_xml()
-    items = parse_pop_xml(count=tweets_count_max)
+    items = parse_pop_xml(collection, count=tweets_count_max)
     for item in items:
-        add_status(item['title'], item['link'])
+        add_status(collection, item['title'], item['link'])
         sleep(1)
+    conn.disconnect()
     print '=================== Process finished %s =========================' % datetime.now().strftime('%Y%m%d %H:%M')
