518748128d6be77ab77ac0a6be627089bad6ef0b
Chi Zhang <clvrobj@gmail.com>
2016-04-02 02:40:27 +0100

    fix post from twitter

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
+Pinboard-Popular
+================
+
 I am a twitter robot to tweet pinboard.in popular bookmarks for you.
 
 Follow me: [@PinPopular](https://twitter.com/#!/PinPopular)
-
-在豆瓣关注我: [豆瓣主页](http://www.douban.com/people/Pinboard/)
--- __init__.py
+++ __init__.py
@@ -5,25 +5,29 @@ from time import sleep
 import random
 import urllib2
 from lxml import etree
-from pymongo import Connection
 from twitter_oauth_op import post2twi
 from douban_oauth_op import post2douban
 
-tweets_count_max = 3
+TWEETS_COUNT_MAX = 3
 
 file_name = 'pinbpopxmltmp.xml'
 xml_keys = {'item': '{http://purl.org/rss/1.0/}item',
             'title': '{http://purl.org/rss/1.0/}title',
-            'link': '{http://purl.org/rss/1.0/}link'}
+            'link': '{http://purl.org/rss/1.0/}link',
+            'description': '{http://purl.org/rss/1.0/}description'}
 pinbpop_url = 'http://feeds.pinboard.in/rss/popular/'
 
 def init_mongo():
-    from local_conf import DATABASE
-    connection = Connection(host=DATABASE['HOST'], port=DATABASE['PORT'], network_timeout=2)
-    db = connection[DATABASE['NAME']]
-    db.authenticate(DATABASE['USER'], DATABASE['PASSWORD'])
-    collection = db['bookmarks']
-    return connection, db, collection
+    try:
+        from pymongo import MongoClient
+        from local_conf import DATABASE
+        client = MongoClient(host=DATABASE['HOST'], port=DATABASE['PORT'])
+        db = client[DATABASE['NAME']]
+        db.authenticate(DATABASE['USER'], DATABASE['PASSWORD'])
+        collection = db['bookmarks']
+        return client, collection
+    except ImportError:
+        return None, None
 
 def check_tweeted_link(collection, link):
     links = collection.find({'link':link})
@@ -44,39 +48,43 @@ def download_pop_xml():
     w.write(data)
     w.close()
 
-def parse_pop_xml(collection, count):
+def parse_pop_xml(max_count, collection):
     doc = etree.parse(file_name)
     items = doc.findall(xml_keys['item'])
     ret = []
     i = 0
-    while len(ret) < count and i < len(items):
+    while len(ret) < max_count and i < len(items):
         item = items[i]
         link = item.find(xml_keys['link']).text.encode('utf-8').strip()
-        if not check_tweeted_link(collection, link):
-            title = item.find(xml_keys['title']).text.encode('utf-8').strip()
-            ret.append({'title':title, 'link':link})
+        if collection and check_tweeted_link(collection, link):
+            i += 1
+            continue
+        title = item.find(xml_keys['title']).text.encode('utf-8').strip()
+        if title == 'Twitter':
+            title = item.find(xml_keys['description']).text.encode('utf-8').strip()
+        ret.append({'title':title, 'link':link})
         i += 1
     return ret
 
-def add_status(collection, title, link):
-    save_tweeted_link(collection, link, title)
+def add_status(title, link, collection):
     content = '%s %s' % (title, link)
     if len(content) > 140:
         tlen = len(content) - len(link) - 1
         content = '%s %s' % (title[:tlen], link)
     print content
     post2twi(content)
-    post2douban(content)
+    # post2douban(content)
+    if collection:
+        save_tweeted_link(collection, link, title)
 
 
 if __name__ == '__main__':
-    conn, db, collection = init_mongo()
     download_pop_xml()
-    items = parse_pop_xml(collection, count=tweets_count_max)
+    conn, collection = init_mongo()
+    items = parse_pop_xml(TWEETS_COUNT_MAX, collection)
     for item in items:
-        add_status(collection, item['title'], item['link'])
+        add_status(item['title'], item['link'], collection)
         sleep(1)
-    conn.disconnect()
+    if conn:
+        conn.disconnect()
     print '=================== Process finished %s =========================' % datetime.now().strftime('%Y%m%d %H:%M')
-    
-    
